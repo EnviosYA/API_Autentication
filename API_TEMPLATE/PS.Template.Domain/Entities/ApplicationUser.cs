53753777b5565b1f3b4acaf21c374f1c1baf1397
Lean Lima <lima.leandro1994@gmail.com>
2020-09-30 20:09:16 -0300

    Agregar token de seguridad

---
+++ API_TEMPLATE/PS.Template.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Template.Domain.Entities
+{
+    public class ApplicationUser: IdentityUser 
+    {
+
+    }
+}
